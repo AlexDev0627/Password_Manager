b2ee5d9743b6d37d76837ee045a49e32dcada83b
AlexDev <yofranksalas@gmail.com>
2026-06-27 08:12:04 -0400

    wrap bottomSheetModal

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,21 +2,24 @@ import { ThemeProvider } from "@/context/ThemeContext";
 import { Stack } from "expo-router";
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
 
 export default function Layout() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <ThemeProvider>
-        <SafeAreaView style={{ flex: 1 }}>
-          <Stack screenOptions={{ headerShown: false }}>
-            <Stack.Screen
-              name="(tabs)"
-              options={{
-                headerShown: false,
-              }}
-            />
-          </Stack>
-        </SafeAreaView>
+        <BottomSheetModalProvider>
+          <SafeAreaView style={{ flex: 1 }}>
+            <Stack screenOptions={{ headerShown: false }}>
+              <Stack.Screen
+                name="(tabs)"
+                options={{
+                  headerShown: false,
+                }}
+              />
+            </Stack>
+          </SafeAreaView>
+        </BottomSheetModalProvider>
       </ThemeProvider>
     </GestureHandlerRootView>
   );
